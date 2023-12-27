1a62e1670a22496e256a83937a0776f6fd0667d5
Victor Enweoru <venweoru@gmail.com>
2023-12-27 20:08:46 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState } from 'react'
+import './pageOne.css'
+
 
 function App() {
+
+  function closeLandingPage () {
+    const page = document.querySelector(".page");
+    page.style.top = "-1000px"
+  }
 
   return (
     <>
       <div className="landingMessage">
-        <div className='One'>
-          <div className="logo"></div>
-          <h1 className='hOne'><span>Boost</span> your chances of landing that <span>dream</span> job</h1>
-          <button className='lBtn'>Create your CV</button>
+        <div className='page'>
+          <div className="pageOne">
+            <div className='One'>
+              <div className="logo"></div>
+              <h1 className='hOne'><span>Boost</span> your chances of landing that <span>dream</span> job</h1>
+              <button
+              className='lBtn'
+              id='landingBtn'
+              onClick={closeLandingPage}
+              >Create your CV</button>
+            </div>
+            <div className='Two'>
+            <div className="lImg swing"></div>
+            <div className="lImg2"></div>
+            </div>
+          </div>
+          <div className="pageTwo">
         </div>
-        <div className='Two'>
-        <div className="lImg"></div>
-        <div className="lImg2"></div>
+
         </div>
       </div>
     </>
