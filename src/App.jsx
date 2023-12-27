87bae053b13a993b98ae199c70aea51159ddadfa
Victor Enweoru <venweoru@gmail.com>
2023-12-27 18:11:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="landingMessage">
+        <div className='One'>
+          <div className="logo"></div>
+          <h1 className='hOne'><span>Boost</span> your chances of landing that <span>dream</span> job</h1>
+          <button className='lBtn'>Create your CV</button>
+        </div>
+        <div className='Two'>
+        <div className="lImg"></div>
+        <div className="lImg2"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
