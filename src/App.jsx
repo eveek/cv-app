0a3927745b3d0a6f359b6776f89d290e1ef2f9b3
Victor Enweoru <venweoru@gmail.com>
2023-12-29 16:17:03 +0100

    Completed form

--- src/App.jsx
+++ src/App.jsx
@@ -32,55 +32,62 @@ function App() {
           <div className="pageTwo">
             <div className='formBox'>
               <form action="">
-                <fieldset>
-                  <input type="text" id='fName' placeholder='Fullname'/>
-                  <div>
-                    <input type="number" id='fPhone' placeholder='Phone'/>
-                    <input type="email" id='fEmail' placeholder='Email'/>
-                  </div>
-                  <div>
-                    <input type="date" id='fDOB' placeholder='Date of Birth' />
-
-                  </div>
-                  <div>
-                    <input type="text" id='fLocation' placeholder='Address' />
-                  </div>
-                </fieldset>
-                <fieldset>
-                  <div>
-                    <input type="text" name="schoolName" id="schollName" placeholder='Name of School' />
-                    <input type="text" name="course" id="course" placeholder='Title of Study' />
-                  </div>
-                  <div>
-                    <div>
-                      <label htmlFor="stdDurationFrom">From</label>
-                      <input type="date" name="stdDurationFrom" id="stdDurationFrom" />
+                <div id="formScroll">
+                  <fieldset id='personal'>
+                    <legend>Personal Information</legend>
+                    <div className='full'><input type="text" id='fName' placeholder='Fullname'/></div>
+                    <div id='phem'>
+                      <input type="number" id='fPhone' placeholder='Phone'/>
+                      <input type="email" id='fEmail' placeholder='Email'/>
                     </div>
                     <div>
-                      <label htmlFor="stdDurationTo">To</label>
-                      <input type="date" name="stdDurationTo" id="stdDurationTo" />
-                    </div>
-                  </div>
-                  <button>+ Add Educational Experience</button>
-                </fieldset>
-                <fieldset>
-                  <div>
-                    <input type="text" name="companyName" id="companyName" placeholder='Name of company' />
-                    <input type="text" name="positionTitle" id="pTitle" placeholder='Position title' />
-                    <input type="text" name="positionResp" id="pResp" placeholder='Main responsibility' />
-                  </div>
-                  <div>
-                    <div>
-                      <label htmlFor="prtDurationFrom">From</label>
-                      <input type="date" name="prtDurationFrom" id="prtDurationFrom" />
+                      <input type="text" id='fLocation' placeholder='Address' />
                     </div>
                     <div>
-                      <label htmlFor="prtDurationTo">To</label>
-                      <input type="date" name="prtDurationTo" id="prtDurationTo" />
+                      <input type="date" id='fDOB' placeholder='Date of Birth' />
                     </div>
-                  </div>
-                  <button>+ Add Work Experience</button>
-                </fieldset>
+                  </fieldset>
+                  <fieldset id='eduExp'>
+                    <legend>Educational Experience</legend>
+                    <div>
+                      <input type="text" name="schoolName" id="schollName" placeholder='Name of School' />
+                    </div>
+                    <div>
+                      <input type="text" name="course" id="course" placeholder='Title of Study' />
+                    </div>
+                    <div className='tDuration'>
+                      <div>
+                        <label htmlFor="stdDurationFrom">From</label>
+                        <input type="date" name="stdDurationFrom" id="stdDurationFrom" />
+                      </div>
+                      <div>
+                        <label htmlFor="stdDurationTo">To</label>
+                        <input type="date" name="stdDurationTo" id="stdDurationTo" />
+                      </div>
+                    </div>
+                    <button>+ Add Educational Experience</button>
+                  </fieldset>
+                  <fieldset>
+                    <legend>Work Experience</legend>
+                    <div>
+                      <div><input type="text" name="companyName" id="companyName" placeholder='Name of company' /></div>
+                      <div><input type="text" name="positionTitle" id="pTitle" placeholder='Position title' /></div>
+                      <div><input type="text" name="positionResp" id="pResp" placeholder='Main responsibility' /></div>
+                    </div>
+                    <div className='tDuration'>
+                      <div>
+                        <label htmlFor="prtDurationFrom">From</label>
+                        <input type="date" name="prtDurationFrom" id="prtDurationFrom" />
+                      </div>
+                      <div>
+                        <label htmlFor="prtDurationTo">To</label>
+                        <input type="date" name="prtDurationTo" id="prtDurationTo" />
+                      </div>
+                    </div>
+                    <button>+ Add Work Experience</button>
+                  </fieldset>
+                </div>
+                <div id="fSubmit"><button id='formSubmit'>Create CV</button></div>
               </form>
             </div>
             <div className='cvDisplay'></div>
