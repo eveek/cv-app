e597ef37369b7040ee9923ae81f1d9826ac32539
Victor Enweoru <venweoru@gmail.com>
2023-12-29 13:54:44 +0100

    Added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './styles/pageOne.css'
 import './styles/pageTwo.css'
 
@@ -32,9 +32,55 @@ function App() {
           <div className="pageTwo">
             <div className='formBox'>
               <form action="">
-                <div></div>
-                <div></div>
-                <div></div>
+                <fieldset>
+                  <input type="text" id='fName' placeholder='Fullname'/>
+                  <div>
+                    <input type="number" id='fPhone' placeholder='Phone'/>
+                    <input type="email" id='fEmail' placeholder='Email'/>
+                  </div>
+                  <div>
+                    <input type="date" id='fDOB' placeholder='Date of Birth' />
+
+                  </div>
+                  <div>
+                    <input type="text" id='fLocation' placeholder='Address' />
+                  </div>
+                </fieldset>
+                <fieldset>
+                  <div>
+                    <input type="text" name="schoolName" id="schollName" placeholder='Name of School' />
+                    <input type="text" name="course" id="course" placeholder='Title of Study' />
+                  </div>
+                  <div>
+                    <div>
+                      <label htmlFor="stdDurationFrom">From</label>
+                      <input type="date" name="stdDurationFrom" id="stdDurationFrom" />
+                    </div>
+                    <div>
+                      <label htmlFor="stdDurationTo">To</label>
+                      <input type="date" name="stdDurationTo" id="stdDurationTo" />
+                    </div>
+                  </div>
+                  <button>+ Add Educational Experience</button>
+                </fieldset>
+                <fieldset>
+                  <div>
+                    <input type="text" name="companyName" id="companyName" placeholder='Name of company' />
+                    <input type="text" name="positionTitle" id="pTitle" placeholder='Position title' />
+                    <input type="text" name="positionResp" id="pResp" placeholder='Main responsibility' />
+                  </div>
+                  <div>
+                    <div>
+                      <label htmlFor="prtDurationFrom">From</label>
+                      <input type="date" name="prtDurationFrom" id="prtDurationFrom" />
+                    </div>
+                    <div>
+                      <label htmlFor="prtDurationTo">To</label>
+                      <input type="date" name="prtDurationTo" id="prtDurationTo" />
+                    </div>
+                  </div>
+                  <button>+ Add Work Experience</button>
+                </fieldset>
               </form>
             </div>
             <div className='cvDisplay'></div>
