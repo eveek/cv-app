e834bce618ed60a2a72aac4a8ad9d379bc51d38c
Victor Enweoru <venweoru@gmail.com>
2024-01-12 23:11:53 +0100

    Updated

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -18,14 +18,114 @@ function Display () {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolore nisi voluptatum! Ut sapiente, modi alias facere sunt illum amet doloribus officiis nemo quos numquam repudiandae voluptatibus ipsum asperiores dolorum.</p>
                         </div>
                         <div>
-                            {/* <div>57 Flower lane, Lagos, Nigeria</div>
-                            <div>justChecking@gmail.com</div>
-                            <div>+23487635728</div> */}
+                            <h2>CONTACT</h2>
+                            <div><p>57 Flower lane, Lagos, Nigeria</p></div>
+                            <div><p>justChecking@gmail.com</p></div>
+                            <div><p>+23487635728</p></div>
+                        </div>
+                        <div>
+                            <h2>EDUCATIONAL</h2>
+                            <div>
+                                <h3>ENTER YOUR DEGREE</h3>
+                                <p>New York University <br />2002 - 2008</p>
+                            </div>
+                            <div>
+                                <h3>ENTER YOUR DEGREE</h3>
+                                <p>New York University <br />2002 - 2008</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h2>SKILLS</h2>
+                            <div>
+                                <ul>
+                                    <div>
+                                        <li>WEB DESIGN</li>
+                                        <li>PHOTOGRAPHY</li>
+                                        <li>SOCIAL MEDIA</li>
+                                    </div>
+                                    <div>
+                                        <li>PUBLIC SPEAKING</li>
+                                        <li>PHOTOGRAPHY</li>
+                                        <li>GRAPHIC DESIGN</li>
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div3">
+                        <div>
+                            <h2>WORK EXPERIENCE</h2>
+                            <div id="wr">
+                                <div className="wrkExp">
+                                    <div className="companyName">
+                                        <h3>ENTER YOUR POSITION HERE</h3>
+                                        <h3>2019 - Present</h3>
+                                    </div>
+                                    <div>
+                                        <p className="faint">Company name, location</p>
+                                        <div className="resp">
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sed similique in et quo nihil accusantium, impedit nostrum provident exercitationem.
+                                            </p>
+                                            <ul>
+                                                <li>
+                                                    <p>Ensured that every website wa deligently accordingly</p>
+                                                </li>
+                                                <li>
+                                                    <p>Ensured that every website wa deligently accordingly</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="wrkExp">
+                                    <div className="companyName">
+                                        <h3>ENTER YOUR POSITION HERE</h3>
+                                        <h3>2019 - Present</h3>
+                                    </div>
+                                    <div>
+                                        <p className="faint">Company name, location</p>
+                                        <div className="resp">
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sed similique in et quo nihil accusantium, impedit nostrum provident exercitationem.
+                                            </p>
+                                            <ul>
+                                                <li>
+                                                    <p>Ensured that every website wa deligently accordingly</p>
+                                                </li>
+                                                <li>
+                                                    <p>Ensured that every website wa deligently accordingly</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="wrkExp">
+                                    <div className="companyName">
+                                        <h3>ENTER YOUR POSITION HERE</h3>
+                                        <h3>2019 - Present</h3>
+                                    </div>
+                                    <div>
+                                        <p className="faint">Company name, location</p>
+                                        <div className="resp">
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sed similique in et quo nihil accusantium, impedit nostrum provident exercitationem.
+                                            </p>
+                                            <ul>
+                                                <li>
+                                                    <p>Ensured that every website wa deligently accordingly</p>
+                                                </li>
+                                                <li>
+                                                    <p>Ensured that every website wa deligently accordingly</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div></div>
-                        <div></div>
                     </div>
-                    <div className="div3"></div>
                 </div>
             </div>
         </>
