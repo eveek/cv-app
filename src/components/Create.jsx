fd86d172785a15c86d380f5b1d96b6aae65812e5
Victor Enweoru <venweoru@gmail.com>
2024-01-16 17:40:03 +0100

    Upddted

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,14 +1,313 @@
-import Form from "./Form"
+import React from "react"
+import { v4 as uuidv4 } from 'uuid'
 import Display from "./Display"
 
-function Create () {
 
-    return (
-        <>
-            <Form/>
-            <Display/>
-        </>
-    )
+import Personal from "./personal"
+import Profile from "./profile"
+import Education from "./education"
+import Work from "./work"
+import Skills from "./skills"
+import Contact from "./contact"
+
+
+class Create extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            personal: [
+                {
+                    name: 'firstname',
+                    text: ''
+                },
+                {
+                    name: 'lastname',
+                    text: ''
+                },
+                {
+                    name: 'profession',
+                    text: ''
+                }
+            ],
+            contact: [
+                {
+                    name: 'phone',
+                    text: ''
+                },
+                {
+                    name: 'email',
+                    text: ''
+                },
+                {
+                    name: 'address',
+                    text: ''
+                },
+            ],
+            profile:[ {
+                    name: 'about',
+                    text: ''
+                }],
+            skills: [
+                {
+                    id: uuidv4(),
+                    skill: ''
+                },
+                {
+                    id: uuidv4(),
+                    skill: ''
+                }
+            ],
+            education: [
+                {
+                    id: uuidv4(),
+                    school: '',
+                    degree: '',
+                    start: '',
+                    end: ''
+                }
+            ],
+            work: [
+                {
+                    id: uuidv4(),
+                    company: '',
+                    position: '',
+                    responsibility: '',
+                    start: '',
+                    end: ''
+                },
+            ],
+        }
+        this.handlePersonalChange = this.handlePersonalChange.bind(this)
+        this.handleEducationChange = this.handleEducationChange.bind(this)
+        this.handleWorkChange = this.handleWorkChange.bind(this)
+        this.handleProfileChange = this.handleProfileChange.bind(this)
+        this.handleContactChange = this.handleContactChange.bind(this)
+        this.handleSkillChange = this.handleSkillChange.bind(this)
+        this.handleWorkAdd = this.handleWorkAdd.bind(this)
+        this.handleEducationAdd = this.handleEducationAdd.bind(this)
+        this.handleSkillAdd = this.handleSkillAdd.bind(this)
+        this.removeWork = this.removeWork.bind(this)
+        this.removeEducation = this.removeEducation.bind(this)
+        this.removeSkill = this.removeSkill.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleEducationChange(event) {
+        const {name, value, id} = event.target
+        const newEdu = this.state.education.map(edu => {
+            if (edu.id == id) {
+                edu[name] = value
+                return edu
+            } else {
+                return edu
+            }
+        })
+        this.setState({
+            ...this.state, education: newEdu
+        })
+    }
+
+    handleWorkChange(event) {
+        const {name, value, id} = event.target
+        const newWrk = this.state.work.map(wrk => {
+            if (wrk.id == id) {
+                wrk[name] = value
+                return wrk
+            } else {
+                return wrk
+            }
+        })
+        this.setState({
+            ...this.state, work: newWrk
+        })
+    }
+
+    handleSkillChange(event) {
+        const {name, value, id} = event.target
+        const newSkill = this.state.skills.map(skill => {
+            if (skill.id == id) {
+                skill[name] = value
+                return skill
+            } else {
+                return skill
+            }
+        })
+        this.setState({
+            ...this.state, skills: newSkill
+        })
+    }
+
+    handlePersonalChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        const newPerson = this.state.personal.map(person => {
+            if (person.name == name) {
+                person.text = value
+                return person
+            } else {
+                return person
+            }
+        })
+        this.setState({
+            ...this.state, personal: newPerson
+        })
+    }
+
+    handleContactChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        const newContact = this.state.contact.map(contact => {
+            if (contact.name == name) {
+                contact.text = value
+                return contact
+            } else {
+                return contact
+            }
+        })
+        this.setState({
+            ...this.state, contact: newContact
+        })
+    }
+
+    handleProfileChange(event) {
+        const value = event.target.value
+        const name = event.target.name
+        const newProfil = this.state.profile.map(profil => {
+            if (profil.name == name) {
+                profil.text = value
+                return profil
+            } else {
+                return profil
+            }
+        })
+        this.setState({
+            ...this.state, profile: newProfil
+        }) 
+    }
+
+    handleWorkAdd(event) {
+        event.preventDefault()
+        this.setState({
+            work: this.state.work.concat({
+                id: uuidv4(),
+                company: '',
+                position: '',
+                responsibility: '',
+                start: '',
+                end: ''
+            })
+        })
+    }
+    handleEducationAdd(event) {
+        event.preventDefault()
+        this.setState({
+            education: this.state.education.concat({
+                id: uuidv4(),
+                school: '',
+                degree: '',
+                start: '',
+                end: ''
+            })
+        })
+    }
+    handleSkillAdd(event) {
+        event.preventDefault()
+        this.setState({
+            skills: this.state.skills.concat({id: uuidv4(), skill: ''})
+        })
+    }
+
+    removeWork(event,index) {
+        event.preventDefault()
+        const list = [...this.state.work]
+        list.splice(index, 1)
+
+        this.setState({
+            work: list
+        })
+    }
+
+    removeEducation(event, index) {
+        event.preventDefault()
+        const list = [...this.state.education]
+        list.splice(index, 1)
+
+        this.setState({
+            education: list
+        })
+    }
+
+    removeSkill(event, index) {
+        event.preventDefault()
+        const list = [...this.state.skills]
+        list.splice(index, 1)
+
+        this.setState({
+            skills: list
+        })
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
+    render() {
+        const {personal, profile, education, work, skills, contact} = this.state
+        return (
+            <>
+                <>
+                    <div className='formBox'>
+                    <form action="">
+                        <div id="formScroll">
+                            <Personal
+                            personal={personal}
+                            onChange={this.handlePersonalChange}
+                            />
+                            <Profile
+                            profile={profile}
+                            onChange={this.handleProfileChange}
+                            />
+                            <Education
+                            education={education}
+                            onChange={this.handleEducationChange}
+                            removeExp={this.removeEducation}
+                            addExp={this.handleEducationAdd}
+                            />
+                            <Work
+                            work={work}
+                            onChange={this.handleWorkChange}
+                            removeWork={this.removeWork}
+                            addWork={this.handleWorkAdd}
+                            />
+                            <Skills
+                            skills={skills}
+                            onChange={this.handleSkillChange}
+                            removeSkill={this.removeSkill}
+                            addSkill={this.handleSkillAdd}
+                            />
+                            <Contact
+                            contact={contact}
+                            onChange={this.handleContactChange}
+                            />
+                        </div>
+                        <div id="fSubmit">
+                            <button onClick={this.handleSubmit} id='formSubmit'>Submit</button>
+                            </div>
+                    </form>
+                    </div>
+                    <Display
+                    personal={personal}
+                    profile={profile}
+                    education={education}
+                    work={work}
+                    skills={skills}
+                    contact={contact}
+                    />
+                </>
+            </>
+        )
+    }
 }
 
 export default Create
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types'
 
 
-function Display () {
+function Display ({personal, profile, education, work, skills, contact}) {
 
     return (
         <>
@@ -8,45 +9,42 @@ function Display () {
                 <div id="cvPaper" className="parent">
                     <div className="div1">
                         <div>
-                            <h1>SOPHIE <span>CLARKE</span></h1>
+                            <h1>{personal[0].text} <span>{personal[1].text}</span></h1>
                         </div>
-                        <div className="title"><h2>PROFESSIONAL @TITLE</h2></div>
+                        <div className="title"><h2>{personal[2].text}</h2></div>
                     </div>
                     <div className="div2">
                         <div>
                             <h2>PROFILE</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolore nisi voluptatum! Ut sapiente, modi alias facere sunt illum amet doloribus officiis nemo quos numquam repudiandae voluptatibus ipsum asperiores dolorum.</p>
+                            <p>{profile[0].text}</p>
                         </div>
                         <div>
                             <h2>CONTACT</h2>
-                            <div><p>57 Flower lane, Lagos, Nigeria</p></div>
-                            <div><p>justChecking@gmail.com</p></div>
-                            <div><p>+23487635728</p></div>
+                            <div><p>{contact[2].text}</p></div>
+                            <div><p>{contact[1].text}</p></div>
+                            <div><p>{contact[0].text}</p></div>
                         </div>
                         <div>
                             <h2>EDUCATIONAL</h2>
-                            <div>
-                                <h3>ENTER YOUR DEGREE</h3>
-                                <p>New York University <br />2002 - 2008</p>
-                            </div>
-                            <div>
-                                <h3>ENTER YOUR DEGREE</h3>
-                                <p>New York University <br />2002 - 2008</p>
-                            </div>
+                            {education.map(edu => {
+                                return (
+                                    <div key={edu.id}>
+                                        <h3>{edu.degree}</h3>
+                                        <p>{edu.school} <br />{edu.start} - {edu.stop}</p>
+                                    </div>
+                                )
+                            })}
                         </div>
                         <div>
                             <h2>SKILLS</h2>
                             <div>
                                 <ul>
                                     <div>
-                                        <li>WEB DESIGN</li>
-                                        <li>PHOTOGRAPHY</li>
-                                        <li>SOCIAL MEDIA</li>
-                                    </div>
-                                    <div>
-                                        <li>PUBLIC SPEAKING</li>
-                                        <li>PHOTOGRAPHY</li>
-                                        <li>GRAPHIC DESIGN</li>
+                                        {skills.map(skill => {
+                                            return (
+                                                <li key={skill.id}>{skill.skill}</li>
+                                            )
+                                        })}
                                     </div>
                                 </ul>
                             </div>
@@ -56,72 +54,24 @@ function Display () {
                         <div>
                             <h2>WORK EXPERIENCE</h2>
                             <div id="wr">
-                                <div className="wrkExp">
-                                    <div className="companyName">
-                                        <h3>ENTER YOUR POSITION HERE</h3>
-                                        <h3>2019 - Present</h3>
-                                    </div>
-                                    <div>
-                                        <p className="faint">Company name, location</p>
-                                        <div className="resp">
-                                            <p>
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sed similique in et quo nihil accusantium, impedit nostrum provident exercitationem.
-                                            </p>
-                                            <ul>
-                                                <li>
-                                                    <p>Ensured that every website wa deligently accordingly</p>
-                                                </li>
-                                                <li>
-                                                    <p>Ensured that every website wa deligently accordingly</p>
-                                                </li>
-                                            </ul>
+                                {work.map(wrk => {
+                                    return (
+                                        <div key={wrk.id} className="wrkExp">
+                                            <div className="companyName">
+                                                <h3>{wrk.position}</h3>
+                                                <h3>{wrk.start} - {wrk.stop}</h3>
+                                            </div>
+                                            <div>
+                                                <p className="faint">{wrk.company}</p>
+                                                <div className="resp">
+                                                    <p>
+                                                        {wrk.responsibility}
+                                                    </p>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="wrkExp">
-                                    <div className="companyName">
-                                        <h3>ENTER YOUR POSITION HERE</h3>
-                                        <h3>2019 - Present</h3>
-                                    </div>
-                                    <div>
-                                        <p className="faint">Company name, location</p>
-                                        <div className="resp">
-                                            <p>
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sed similique in et quo nihil accusantium, impedit nostrum provident exercitationem.
-                                            </p>
-                                            <ul>
-                                                <li>
-                                                    <p>Ensured that every website wa deligently accordingly</p>
-                                                </li>
-                                                <li>
-                                                    <p>Ensured that every website wa deligently accordingly</p>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="wrkExp">
-                                    <div className="companyName">
-                                        <h3>ENTER YOUR POSITION HERE</h3>
-                                        <h3>2019 - Present</h3>
-                                    </div>
-                                    <div>
-                                        <p className="faint">Company name, location</p>
-                                        <div className="resp">
-                                            <p>
-                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sed similique in et quo nihil accusantium, impedit nostrum provident exercitationem.
-                                            </p>
-                                            <ul>
-                                                <li>
-                                                    <p>Ensured that every website wa deligently accordingly</p>
-                                                </li>
-                                                <li>
-                                                    <p>Ensured that every website wa deligently accordingly</p>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
+                                    )
+                                })}
                             </div>
                         </div>
                         <div></div>
@@ -130,6 +80,15 @@ function Display () {
             </div>
         </>
     )
+}
+
+Display.propTypes = {
+    personal: PropTypes.array,
+    profile: PropTypes.array,
+    contact: PropTypes.array,
+    work: PropTypes.array,
+    education: PropTypes.array,
+    skills: PropTypes.array
 }
 
 export default Display
